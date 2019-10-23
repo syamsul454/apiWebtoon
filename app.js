481c8417796d6bee0045ff89c8950088c26789d8
syamsul454 <andibrigjent@gmail.com>
2019-10-23 11:45:24 +0700

    perubahan pada port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 require('dotenv').config();
 require('express-group-routes')
 const app = express()
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
